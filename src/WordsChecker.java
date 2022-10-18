dab878968407093675e0c64a1418ebee7b01d48a
Dmitriy-Kamenskiy <webhd@yandex.ru>
2022-10-18 15:15:48 +0300

    Initial commit

---
+++ src/WordsChecker.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class WordsChecker{
+    protected String text;
+    public Set<String> wordSet;
+
+    public WordsChecker(String text) {
+        this.text = text;
+        wordSet = new HashSet<>();
+        String parts[] = this.text.split("\\P{IsAlphabetic}+");
+        for (String part : parts) {
+            wordSet.add(part);
+        }
+    }
+
+
+    public boolean hasWord(String word){
+        boolean result = wordSet.contains(word);
+        if (result) {
+            return true;
+        }
+        return false;
+    }
+}
